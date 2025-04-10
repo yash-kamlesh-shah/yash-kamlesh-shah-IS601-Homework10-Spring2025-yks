b135c6a79e53ace5c9d3d09a776be13805ccf054
yash-kamlesh-shah <yks@njit.edu>
2025-04-10 19:01:15 -0400

    updated code with 99% coverage

--- app/services/jwt_service.py
+++ app/services/jwt_service.py
@@ -4,15 +4,25 @@ import jwt
 from datetime import datetime, timedelta
 from settings.config import settings
 
+# def create_access_token(*, data: dict, expires_delta: timedelta = None):
+#     to_encode = data.copy()
+#     # Convert role to uppercase before encoding the JWT
+#     if 'role' in to_encode:
+#         to_encode['role'] = to_encode['role'].upper()
+#     expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=settings.access_token_expire_minutes))
+#     to_encode.update({"exp": expire})
+#     encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
+#     return encoded_jwt
 def create_access_token(*, data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    # Convert role to uppercase before encoding the JWT
+    # Convert role to string and then to uppercase before encoding the JWT
     if 'role' in to_encode:
-        to_encode['role'] = to_encode['role'].upper()
+        to_encode['role'] = str(to_encode['role']).upper()  # Convert role to string first
     expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=settings.access_token_expire_minutes))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, algorithm=settings.jwt_algorithm)
     return encoded_jwt
+
 
 def decode_token(token: str):
     try:
